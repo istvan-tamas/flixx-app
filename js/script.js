84044ce8e65245ed03c5728d171a6cb52bddc3eb
istvan-tamas <istvan.tamas90@gmail.com>
2025-01-13 12:04:17 +0100

    API query implemented

--- js/script.js
+++ js/script.js
@@ -2,6 +2,23 @@ const state = {
 	currentPage: window.location.pathname,
 };
 
+async function displayPopMovies() {
+	const results = await fetchAPIData('movie/popular');
+	console.log(results);
+}
+
+async function fetchAPIData(endpoint) {
+	const API_KEY = '6dd66f7277fc9eb98e90301a5073dbfe';
+	const API_URL = 'https://api.themoviedb.org/3/';
+
+	const response = await fetch(
+		`${API_URL}${endpoint}?api_key=${API_KEY}&language=hu-HU`
+	);
+
+	const data = await response.json();
+	return data;
+}
+
 highLightLink = () => {
 	const navLinks = document.querySelectorAll('.nav-link');
 	navLinks.forEach((link) => {
@@ -15,6 +32,7 @@ init = () => {
 	switch (state.currentPage) {
 		case '/':
 		case '/index.html':
+			displayPopMovies();
 			console.log('home');
 			break;
 		case '/shows.html':
