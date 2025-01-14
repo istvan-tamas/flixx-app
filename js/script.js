42ecc333a48ffcb8e683f1919eba7f0674710ea2
istvan-tamas <istvan.tamas90@gmail.com>
2025-01-14 10:20:58 +0100

    Adding movie details

--- js/script.js
+++ js/script.js
@@ -66,6 +66,61 @@ async function displayTwenty() {
 	});
 }
 
+async function displayMovieDetails() {
+	const movieID = window.location.search.split('=')[1];
+	const movie = await fetchAPIData(`movie/${movieID}`);
+
+	const div = document.createElement('div');
+	div.innerHTML = `<div class="details-top">
+          <div>
+             ${
+								movie.poster_path
+									? `<img
+							src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+							class="card-img-top"
+							alt="${movie.title}"
+						/>`
+									: `<img
+							src="images/no-image.jpg"
+							class="card-img-top"
+							alt="${movie.title}"
+						/>`
+							}
+          </div>
+          <div>
+            <h2>${movie.title}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+${movie.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">Release Date: ${movie.release_date}</p>
+            <p>
+              ${movie.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+				${movie.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+            </ul>
+            <a href="${
+							movie.homepage
+						}" target="_blank" class="btn">Visit Movie Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Movie Info</h2>
+          <ul>
+            <li><span class="text-secondary">Budget:</span>${movie.budget}</li>
+            <li><span class="text-secondary">Revenue:</span> $2,000,000</li>
+            <li><span class="text-secondary">Runtime:</span> 90 minutes</li>
+            <li><span class="text-secondary">Status:</span> Released</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">Company 1, Company 2, Company 3</div>
+        </div>
+		`;
+	document.querySelector('#movie-details').appendChild(div);
+}
+
 async function fetchAPIData(endpoint) {
 	const API_KEY = '6dd66f7277fc9eb98e90301a5073dbfe';
 	const API_URL = 'https://api.themoviedb.org/3/';
@@ -110,6 +165,7 @@ init = () => {
 			console.log('shows');
 			break;
 		case '/movie-details.html':
+			displayMovieDetails();
 			console.log('movie details');
 			break;
 		case '/tv-details.html':
