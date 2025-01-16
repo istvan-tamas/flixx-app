e0bd4d9873f6da0cb675fb502a0518c3694d3449
istvan-tamas <istvan.tamas90@gmail.com>
2025-01-16 18:25:15 +0100

    Swiper added

--- js/script.js
+++ js/script.js
@@ -221,19 +221,48 @@ displayBackgroundImage = (type, backgroundPath) => {
 async function displaySlider() {
 	const { results } = await fetchAPIData('movie/now_playing');
 
-	results.forEach((result) => {
+	results.forEach((movie) => {
 		const div = document.createElement('div');
 		div.classList.add('swiper-slide');
 		div.innerHTML = `
-						<a href="movie-details.html?id=1">
-							<img src="./images/no-image.jpg" alt="Movie Title" />
+						<a href="movie-details.html?id=${movie.id}">
+							<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${
+			movie.title
+		}" />
 						</a>
 						<h4 class="swiper-rating">
-							<i class="fas fa-star text-secondary"></i> 8 / 10
+							<i class="fas fa-star text-secondary"></i> ${movie.vote_average.toFixed(1)} / 10
 						</h4>
 		`;
+
+		document.querySelector('.swiper-wrapper').appendChild(div);
+		initSwiper();
 	});
 }
+
+initSwiper = () => {
+	const swiper = new Swiper('.swiper', {
+		slidesPerView: 1,
+		spaceBetween: 30,
+		freeMode: true,
+		loop: true,
+		autoplay: {
+			delay: 4000,
+			disableOnInteraction: false,
+		},
+		breakpoints: {
+			500: {
+				slidesPerView: 2,
+			},
+			700: {
+				slidesPerView: 3,
+			},
+			1200: {
+				slidesPerView: 4,
+			},
+		},
+	});
+};
 
 async function fetchAPIData(endpoint) {
 	const API_KEY = '6dd66f7277fc9eb98e90301a5073dbfe';
